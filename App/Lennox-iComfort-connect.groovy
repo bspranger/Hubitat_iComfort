1ed02124e7b6f13c4a197b8cec3b972542ae8cb4
bspranger <brianspranger@hotmail.com>
2019-01-05 22:13:46 -0600

    added GitHub Links
    The GitHub links will allow easy update by copying and pasting it into the Hubitat import.

--- App/Lennox-iComfort-connect.groovy
+++ App/Lennox-iComfort-connect.groovy
@@ -4,7 +4,7 @@
  *  Copyright 2015 Jason Mok
  *
  *  GitHub Link
- *  
+ *  https://raw.githubusercontent.com/bspranger/Hubitat_iComfort/master/App/Lennox-iComfort-connect.groovy
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
